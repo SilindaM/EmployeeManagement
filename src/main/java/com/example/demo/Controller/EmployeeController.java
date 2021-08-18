37d0895be6442bb0453f4db580128273477b4a06
unknown <silindaM>
2021-08-18 04:08:42 +0200

    Add Cross origin and Request mapping

--- src/main/java/com/example/demo/Controller/EmployeeController.java
+++ src/main/java/com/example/demo/Controller/EmployeeController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +21,8 @@ import com.example.demo.Model.Employee;
 import com.example.demo.Repository.EmployeeRepository;
 
 @RestController
-@RequestMapping("/api/")
+@RequestMapping("/api/employees")
+@CrossOrigin(origins="http://localhost:8081")
 public class EmployeeController {
 
 	@Autowired
