8a97527e3087cff071acfa043091336a35894af8
silindaM <silindanemdu@gmail.com>
2021-08-19 15:17:13 +0200

    push employee list

--- FrontEnd/src/app/Components/employee-list/employee-list.component.ts
+++ FrontEnd/src/app/Components/employee-list/employee-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Employee } from 'Model/employee';
+import { Observable } from 'rxjs';
+import { EmployeeServiceService } from 'src/app/Services/employee-service.service';
 
 @Component({
   selector: 'app-employee-list',
@@ -6,10 +10,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee-list.component.css']
 })
 export class EmployeeListComponent implements OnInit {
-
-  constructor() { }
+  employees?:Observable<Employee[]>;
+  constructor(private employeeService:EmployeeServiceService,private router:Router) { }
 
   ngOnInit(): void {
+    //when the load call this method
+    this.loadEmployees();
+  }
+  //load employees
+  loadEmployees(){
+    this.employees=this.employeeService.getAllEmployees();
+  }
+  //delete employee by id
+  deleteEmployeeById(id:number){
+    this.employeeService.deleteEmployeeById(id)
+    .subscribe(
+      data=>{
+        console.log(data);
+        //load the employees not deleted after deleting
+        this.loadEmployees();
+      },
+      error=>{
+        console.log(error);
+      });
+  }
+  //show employee details
+  showEmployeeDetails(id:number){
+    this.router.navigate(['details',id]);
+  }
+  //update Employees Details
+  updateEmployeeDetails(id:number){
+    this.router.navigate(['update',id]);
   }
 
 }
