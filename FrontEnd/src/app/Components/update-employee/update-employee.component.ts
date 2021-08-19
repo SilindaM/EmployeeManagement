610d8d5d220b48358568f937d393f0a2032e1604
silindaM <silindanemdu@gmail.com>
2021-08-19 20:31:56 +0200

    update employee file

--- FrontEnd/src/app/Components/update-employee/update-employee.component.ts
+++ FrontEnd/src/app/Components/update-employee/update-employee.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Employee } from 'src/app/Model/employee';
+import { EmployeeServiceService } from 'src/app/Services/employee-service.service';
 
 @Component({
   selector: 'app-update-employee',
@@ -6,10 +9,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-employee.component.css']
 })
 export class UpdateEmployeeComponent implements OnInit {
+  id: number=0;
+  employee?:Employee;
 
-  constructor() { }
+  constructor(private employeeService:EmployeeServiceService,private router:Router,private activatedRoute:ActivatedRoute) { }
 
   ngOnInit(): void {
+    //when the application loads, load the selected id of the employee
+    this.employee=new Employee();
+    this.id=this.activatedRoute.snapshot.params['id'];
+    this.employeeService.getEmployee(this.id)
+    .subscribe(
+      data=>{
+        console.log(data);
+        this.employee=data;
+      },
+      error=>{
+        console.log(error);
+      });
   }
-
+  //update the employee
+  updateEmployee(){
+    this.employeeService.updateEmployee(this.id,this.employee)
+    .subscribe(
+      data=>{
+        console.log(data);
+        this.employee=new Employee();
+        this.loadEmployees();
+      },
+      error=>{
+        console.log(error)
+      }
+    )
+  }
+  //load employees
+  loadEmployees(){
+    this.router.navigate(['/employees']);
+  }
+  //submit button
+  onSubmitButton(){
+    this.updateEmployee();
+  }
 }
