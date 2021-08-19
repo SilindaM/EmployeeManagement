51b9b45cd04b6787559b69c9fa0e3703224ca5fd
silindaM <silindanemdu@gmail.com>
2021-08-19 21:18:59 +0200

    change the redirectory of componets

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CreateEmployeeComponent } from './Model/Components/create-employee/create-employee.component';
-import { EmployeeDetailsComponent } from './Model/Components/employee-details/employee-details.component';
-import { EmployeeListComponent } from './Model/Components/employee-list/employee-list.component';
-import { UpdateEmployeeComponent } from './Model/Components/update-employee/update-employee.component';
+import { CreateEmployeeComponent } from './Components/create-employee/create-employee.component';
+import { EmployeeDetailsComponent } from './Components/employee-details/employee-details.component';
+import { EmployeeListComponent } from './Components/employee-list/employee-list.component';
+import { UpdateEmployeeComponent } from './Components/update-employee/update-employee.component';
 
 @NgModule({
   declarations: [
