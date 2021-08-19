6939f6aa6d96591b2c320b5e4e43cf2b7230ff70
silindaM <silindanemdu@gmail.com>
2021-08-19 13:55:18 +0200

    create employee file

--- FrontEnd/src/app/Components/create-employee/create-employee.component.ts
+++ FrontEnd/src/app/Components/create-employee/create-employee.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Employee } from 'Model/employee';
+import { EmployeeServiceService } from 'src/app/Services/employee-service.service';
 
 @Component({
   selector: 'app-create-employee',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateEmployeeComponent implements OnInit {
 
-  constructor() { }
+  //employee object
+  employee:Employee=new Employee();
+  submitted=false;
+
+  constructor(private employeeService:EmployeeServiceService,private router:Router) { }
 
   ngOnInit(): void {
+  }
+  //new employee create
+  newEmployee():void{
+    this.submitted=false;
+    this.employee=new Employee();
+  }
+  //save employee
+  save(){
+    this.employeeService.createEmployee(this.employee)
+    .subscribe(data=>{
+      console.log(data);
+      this.employee=new Employee();
+      this.returnToList();
+    },
+    error=>{
+      console.log(error);
+    });
+  }
+  //return to list
+  returnToList(){
+    this.router.navigate(['/employees']);
   }
 
 }
