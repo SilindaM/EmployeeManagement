b9a5c7811150762da0498dc8a0679cf4035e164b
silindaM <silindanemdu@gmail.com>
2021-08-19 20:32:22 +0200

    add router outlets

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateEmployeeComponent } from './Components/create-employee/create-employee.component';
+import { EmployeeDetailsComponent } from './Components/employee-details/employee-details.component';
+import { EmployeeListComponent } from './Components/employee-list/employee-list.component';
+import { UpdateEmployeeComponent } from './Components/update-employee/update-employee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:"employee",pathMatch:"full"},
+  {path:"employees",component:EmployeeListComponent},
+  {path:"add",component:CreateEmployeeComponent},
+  {path:"update/:id",component:UpdateEmployeeComponent},
+  {path:"details/:id",component:EmployeeDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
