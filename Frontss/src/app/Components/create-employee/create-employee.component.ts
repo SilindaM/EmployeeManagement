f9db44c36fb3ac514be25e2e1a95d21d5d22a787
silindaM <silindanemdu@gmail.com>
2021-08-25 14:03:29 +0200

    Create a new employee

--- Frontss/src/app/Components/create-employee/create-employee.component.ts
+++ Frontss/src/app/Components/create-employee/create-employee.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Employee } from 'src/app/Model/employee';
+import { EmployeesService } from 'src/app/Services/employees.service';
 
 @Component({
   selector: 'app-create-employee',
@@ -6,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-employee.component.css']
 })
 export class CreateEmployeeComponent implements OnInit {
+  employee:Employee=new Employee();
+  submitted=false;
 
-  constructor() { }
+  constructor(private servi:EmployeesService,private router:Router) { }
 
   ngOnInit(): void {
+  }
+  //create new employee
+  newEmployee():void{
+    this.submitted=false;
+    this.employee=new Employee();
+  }
+  save(){
+      this.servi.createEmployee(this.employee).subscribe(
+        data=>{
+          console.log(data);
+        },error=>{
+          console.log(error);
+        });
+        //create the employee
+        this.employee=new Employee();
+        //go back to the list
+        this.BackToList();
+  }
+  //go back to the list
+  BackToList(){
+    this.router.navigate(['/employees']);
+  }
+  //when clicking the submit button
+  onSubmit(){
+    this.submitted=true;
+    this.save();
   }
 
 }
