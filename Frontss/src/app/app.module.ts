52e9381e05e1a041ae1d82aea7f13053571fefe6
silindaM <silindanemdu@gmail.com>
2021-08-25 20:42:34 +0200

    Add routerModule global

--- Frontss/src/app/app.module.ts
+++ Frontss/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {  HttpClientModule} from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeListComponent } from './Components/employee-list/employee-list.component';
@@ -18,7 +18,8 @@ import { EmployeeDetailsComponent } from './Components/employee-details/employee
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
