5f1e4fa8c4ecd7ffbd1891eed2ff1b405f8564d7
silindaM <silindanemdu@gmail.com>
2021-08-25 13:27:55 +0200

    create Model and Components

--- Frontss/src/app/app.module.ts
+++ Frontss/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeListComponent } from './Components/employee-list/employee-list.component';
+import { CreateEmployeeComponent } from './Components/create-employee/create-employee.component';
+import { UpdateEmployeeComponent } from './Components/update-employee/update-employee.component';
+import { EmployeeDetailsComponent } from './Components/employee-details/employee-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeListComponent,
+    CreateEmployeeComponent,
+    UpdateEmployeeComponent,
+    EmployeeDetailsComponent
   ],
   imports: [
     BrowserModule,
